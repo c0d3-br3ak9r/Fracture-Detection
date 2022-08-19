f23dcfa6e086d49644a6088364ec3d4cd3af36a1
Subhi2002 <75176547+Subhi2002@users.noreply.github.com>
2022-08-19 21:05:02 +0530

    before adding buttns

--- app/src/main/java/com/example/myapplication/elbow.java
+++ app/src/main/java/com/example/myapplication/elbow.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,32 +11,25 @@ public class elbow extends AppCompatActivity {
 
     int[] newArray;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        newArray= new int[]{
-                R.id.ex1,R.id.ex2,R.id.ex3,R.id.ex4,R.id.ex5
+        newArray = new int[]{
+                R.id.ex2, R.id.ex3, R.id.ex4, R.id.ex5, R.id.ex6
         };
     }
 
-
-    public void ImagebuttonClicked(View view){
-
-        for(int i=0;i< newArray.length;i++){
-            if(view.getId()== newArray[i]){
-               int value=i+1;
-               // Log.i("FIRST",String.copyValueOf(value));
-                Log.i("FIRST",String.copyValueOf(value));
-                Intent intent=new Intent(elbow.this,elboweach.class);
+    public void Imagebuttonclicked(View view) {
+        for (int i = 0; i < newArray.length; i++) {
+            if (view.getId() == newArray[i]) {
+                int value = i + 1;
+                Log.i("FIRST", String.valueOf(value));
+                Intent intent = new Intent(elbow.this, elboweach.class);
                 startActivity(intent);
 
             }
         }
-
     }
 }
